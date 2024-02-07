ed2a720995d6213dd7430f661958b99fb45ab253
jiwonjeon <j.jeon339@mybvc.ca>
2024-02-06 22:33:06 -0700

    Added resources

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Layout()
                 }
             }
         }
@@ -30,17 +49,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtContainer(){
+   //Image(painter = , contentDescription = )
+}
+
+@Composable
+fun ArtworkDes(artwork : Int){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+
+    ) {
+        Text(text = "Artwork Title")
+        Text(text = "Artwork Artist(Year)")
+    }
+
+}
+
+@Composable
+fun Buttons(){
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            //colors = ButtonDefaults.buttonColors(Color.LightGray),
+            //shape = RectangleShape,
+            modifier = Modifier.height(40.dp).width(160.dp)
+            ) {
+            Text(text = "Previous")
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            //colors = ButtonDefaults.buttonColors(Color.Gray),
+            //shape = RectangleShape,
+            modifier = Modifier.height(40.dp).width(160.dp)
+        ) {
+            Text(text = "Next")
+
+        }
+    }
+}
+
+@Composable
+fun Layout() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        ArtContainer()
+        ArtworkDes(1)
+        Buttons()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceAppTheme {
-        Greeting("Android")
+        Layout()
     }
 }
